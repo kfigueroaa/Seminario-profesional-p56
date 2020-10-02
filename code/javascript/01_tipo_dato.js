6a01ad5c43da3727942d0c53238399236d71239f
Kerlly Figueroa <kfigueroaa@est.ups.edu.ec>
2020-10-02 16:13:13 -0500

    Implementacion de Promesas y Async Await.

---
+++ code/javascript/01_tipo_dato.js
@@ -0,0 +1,1 @@
+//Se trabajo directamente desde la consola del navegador
---
+++ code/javascript/02_String.js
@@ -0,0 +1,20 @@
+var nombre = "Guillermo"
+var apellido = "Pizarro"
+
+var nombre = "guillermo", apellido = "pizarro"
+
+var nombreMayuscula = nombre.toUpperCase()
+var apellidoMayuscula = apellido.toUpperCase()
+
+console.log( nombreMayuscula + " "+ apellidoMayuscula)
+//Espiciicacion de Javascript EcnaScript +6
+console.log(`${nombre} ${apellidoMayuscula}`)
+
+var primeraLetraNombre = nombre.charAt(0)
+var longitudNombre = nombre.length
+var subcadenaNombre = nombre.substr(1,3)
+
+console.log( primeraLetraNombre)
+console.log(longitudNombre)
+console.log(subcadenaNombre)
+
